c3d629858cd719f2695db0fb000238b323295cde
Gabriel Belém Lima <eugabrielbelem@gmail.com>
2026-05-26 18:54:19 -0300

    ex027 - flex013 - Projeto Display Flex Finalizado

--- exercicios/ex027/flex013-menu/script.js
+++ exercicios/ex027/flex013-menu/script.js
@@ -9,7 +9,7 @@ function verMenu() {
 }
 
 function mudouTamanho() {
-    if (window.innerWidth >= 530) {
+    if (window.innerWidth >= 667) {
         navmenu.style.display = 'block'
         menu.innerHTML = 'menu'
     } else {
@@ -17,4 +17,58 @@ function mudouTamanho() {
         menu.innerHTML = 'menu'
 
     }
+}
+
+function sBasic() {
+    basic.style.display = 'block'
+    justifycont.style.display = 'none'
+    justifyitems.style.display = 'none'
+    alignitems.style.display = 'none'
+    flexflow.style.display = 'none'
+    flex.style.display = 'none'
+}
+function sJustifycont() {
+    basic.style.display = 'none'
+    justifycont.style.display = 'block'
+    justifyitems.style.display = 'none'
+    alignitems.style.display = 'none'
+    flexflow.style.display = 'none'
+    flex.style.display = 'none'
+}
+function sJustifyitems() {
+    basic.style.display = 'none'
+    justifycont.style.display = 'none'
+    justifyitems.style.display = 'block'
+    alignitems.style.display = 'none'
+    flexflow.style.display = 'none'
+    flex.style.display = 'none'
+}
+function sAlignitems() {
+    basic.style.display = 'none'
+    justifycont.style.display = 'none'
+    justifyitems.style.display = 'none'
+    alignitems.style.display = 'block'
+    flexflow.style.display = 'none'
+    flex.style.display = 'none'
+}
+function sFlexflow() {
+    basic.style.display = 'none'
+    justifycont.style.display = 'none'
+    justifyitems.style.display = 'none'
+    alignitems.style.display = 'none'
+    flexflow.style.display = 'block'
+    flex.style.display = 'none'
+}
+function sFlex() {
+    basic.style.display = 'none'
+    justifycont.style.display = 'none'
+    justifyitems.style.display = 'none'
+    alignitems.style.display = 'none'
+    flexflow.style.display = 'none'
+    flex.style.display = 'block'
+}
+
+function setActive(el) {
+    document.querySelectorAll('#navmenu li').forEach(li => li.classList.remove('active'));
+    el.classList.add('active');
 }
